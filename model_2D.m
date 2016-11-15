8173b0af50b41f9a71e4940508a332c3b753b5e5
Vojtech Vladyka <vojtech.vladyka@gmail.com>
2016-11-15 17:43:50 +0100

    Cellarray of 2D models

---
+++ model_2D.m
@@ -0,0 +1,5 @@
+model = cell(4,1);
+model(1) = {@(x,xdata)x(1)+x(2)*xdata.^1}; 
+model(2) = {@(x,xdata)x(1)+x(2)*xdata.^1+x(3)*xdata.^2}; 
+model(3) = {@(x,xdata)x(1)+x(2)*xdata.^1+x(3)*xdata.^2+x(4)*xdata.^3}; 
+model(4) = {@(x,xdata)x(1)+x(2)*xdata.^1+x(3)*xdata.^2+x(4)*xdata.^3+x(5)*xdata.^4};
