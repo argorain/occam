7f4fceac8203050971fec81f272d2e5900f54d6b
Vojtech Vladyka <vojtech.vladyka@gmail.com>
2016-12-03 12:35:15 +0100

    Reworked ls model db to cell array

--- model_2D.m
+++ model_2D.m
@@ -1,7 +1,12 @@
-model = cell(4,1);
-model(1) = {@(x,xdata)x(1)+x(2)*xdata.^1}; 
-model(2) = {@(x,xdata)x(1)+x(2)*xdata.^1+x(3)*xdata.^2}; 
-model(3) = {@(x,xdata)x(1)+x(2)*xdata.^1+x(3)*xdata.^2+x(4)*xdata.^3}; 
-model(4) = {@(x,xdata)x(1)+x(2)*xdata.^1+x(3)*xdata.^2+x(4)*xdata.^3+x(5)*xdata.^4};
+model = cell(4,2); %first cell is function, second cell is x dimension
+model(1,1) = {@(x,xdata)x(1)+x(2)*xdata.^1}; 
+model(2,1) = {@(x,xdata)x(1)+x(2)*xdata.^1+x(3)*xdata.^2}; 
+model(3,1) = {@(x,xdata)x(1)+x(2)*xdata.^1+x(3)*xdata.^2+x(4)*xdata.^3}; 
+model(4,1) = {@(x,xdata)x(1)+x(2)*xdata.^1+x(3)*xdata.^2+x(4)*xdata.^3+x(5)*xdata.^4};
+
+model(1,2) = {2};
+model(2,2) = {3};
+model(3,2) = {4};
+model(4,2) = {5};
 
 solve_lsq = @(fn,x0,xdata,ydata)lsqcurvefit(fn,x0,xdata,ydata);
--- runme.m
+++ runme.m
@@ -21,15 +21,16 @@ addpath('fun');
 %% Model fitting (Least square)
 disp('Model  fitting...')
 
-x0 = [1,1,1,1,1,1,1,1,1]; % start parameters, amount results in polynom length
-ls = zeros(length(model), length(x0));
+ls = cell(length(model), 1);
 for i = [1:length(model)]
     fn = model{i};
+    x0 = ones(1,model{i,2});
+    
     % LS fitting
     if dimension == 2
-        ls(i,:) = solve_lsq(fn,x0,noise(1,:),noise(2,:));
+        ls(i) = {solve_lsq(fn,x0,noise(1,:),noise(2,:))};        
     else
-        ls(i,:) = solve_lsq(fn,x0,in_data,noise);
+        ls(i) = {solve_lsq(fn,x0,in_data,noise)};
     end
     % plot of results
     %plot(noise(1,:),fn(ls(i,:),noise(1,:)), '-black')
@@ -46,9 +47,9 @@ P = zeros(length(model), 1);
 for i = [1:length(model)]
     fn = model{i};
     if dimension == 2
-        P(i) = 1/n * sum((fn(ls(i,:),noise(1,:)) - noise(2,:)).^2);
+        P(i) = 1/n * sum((fn(ls{i},noise(1,:)) - noise(2,:)).^2);
     else
-        P(i) = model_eval_sq(ls(i,:),fn,in_data, noise);
+        P(i) = model_eval_sq(ls{i},fn,in_data, noise);
     end
     
     % show them
@@ -62,14 +63,14 @@ disp('Results')
 fn = model{pos};
 fprintf('Selected model #%d\n', pos)
 fprintf('Parameters')
-ls(pos,:)
+ls{pos}
 
 if dimension == 2
     figure;
     hold on;
     %plot(data(1,:), data(2,:), '.r')   
     plot(noise(1,:), noise(2,:), '.b')
-    plot(noise(1,:),fn(ls(pos,:),noise(1,:)), '-black')
+    plot(noise(1,:),fn(ls{pos},noise(1,:)), '-black')
     grid on;
     title('Model fitting, 2D data')
     xlabel('x')
@@ -79,7 +80,7 @@ elseif dimension == 3
     hold on;
     %plot3(repmat(in_data(1,:),size(in_data,2),1), repmat(in_data(2,:),size(in_data,2),1)', data, '.r')  
     plot3(repmat(in_data(1,:),size(in_data,2),1), repmat(in_data(2,:),size(in_data,2),1)', noise, '.b')
-    plot3(repmat(in_data(1,:),size(in_data,2),1), repmat(in_data(2,:),size(in_data,2),1)', fn(ls(pos,:),in_data), '-k')
+    plot3(repmat(in_data(1,:),size(in_data,2),1), repmat(in_data(2,:),size(in_data,2),1)', fn(ls{pos},in_data), '-k')
     grid on;
     title('Model fitting, 3D data')
     xlabel('x')
